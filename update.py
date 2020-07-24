19487cdaad97f57cdf7e1be4197add0ba265c939
KatherineMichel <kthrnmichel@gmail.com>
2020-07-23 20:11:22 -0500

    Update update.py

--- update.py
+++ update.py
@@ -30,6 +30,10 @@ FOOTER="""\
 * Add total TILs
 * Humanize "Tils" plural
 * Create GitHub template
+
+## License
+
+Adapted from a [Today I Learned](https://github.com/khanhicetea/today-i-learned/) project by [@khanhicetea](https://github.com/khanhicetea), distributed under the [Creative Commons Attribution License](http://creativecommons.org/licenses/by/3.0/). See my [TIL- 100 Days of Code Version](https://github.com/KatherineMichel/portfolio/blob/master/regular-blog-posts/til-100-days-of-code-version.md) blog post for information about the changes I made.
 """
 
 def main():
