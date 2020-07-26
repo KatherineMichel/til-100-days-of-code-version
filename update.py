a51ba61b9a278348d28a8bb68364a9deba51f24a
KatherineMichel <kthrnmichel@gmail.com>
2020-07-26 17:11:29 -0500

    Update update.py

--- update.py
+++ update.py
@@ -23,14 +23,6 @@ See my [TIL- 100 Days of Code Version](https://github.com/KatherineMichel/portfo
 
 FOOTER = """\
 
-## ToDo
-
-* Add `parse_til()` notes to [program_information.py](program_information.py)
-* Fill in header and footer
-* Add total TILs
-* Humanize "Tils" plural
-* Create GitHub template
-
 ## License
 
 This project is adapted from a [Today I Learned](https://github.com/khanhicetea/today-i-learned/) project by [@khanhicetea](https://github.com/khanhicetea), distributed under the [Creative Commons Attribution License](http://creativecommons.org/licenses/by/3.0/). 
